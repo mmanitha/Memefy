c806b53ef4a43df9e2283aeb157d187970173826
mmanitha <michael.manisa@gmail.com>
2016-12-13 00:12:33 -0500

    Initial Commit

---
+++ Memefy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memefy
+//
+//  Created by Michael Manisa on 12/13/16.
+//  Copyright © 2016 Michael Manisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
