3fbf4e7a59bd12b3abc7c9ba8c6a297adfff4a52
mmanitha <michael.manisa@gmail.com>
2016-12-14 00:18:36 -0500

    Set Up
    add basic UI elements
    add image picking functionality

--- Memefy/ViewController.swift
+++ Memefy/ViewController.swift
@@ -8,10 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: Outlets
+    
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    
+    //MARK: Properties
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .black
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +34,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 
+    //Pick from Photo Library
+    @IBAction func pickImageFromAlbum(_ sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    
+    //Pick from Camera
+    @IBAction func pickImageFromCamera(_ sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        present(picker, animated: true, completion: nil)
+    }
+    
 
 }
 
